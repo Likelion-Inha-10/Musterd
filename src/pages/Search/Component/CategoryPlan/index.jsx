07ce5dfa5b5dfa4555cf686653c56ff156db3f3b
JunHyeon <dlwnsgus07@naver.com>
2022-08-12 17:29:31 +0900

    feat: add CategoryPlan Component

---
+++ src/pages/Search/Component/CategoryPlan/index.jsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+const Wrapper = styled.div`
+  background-color: red;
+`;
+const Image = styled.img`
+  width: 30px;
+  height: 30px;
+`;
+const CategoryPlan = ({ category, place, writer, benefit, image }) => {
+  return (
+    <Wrapper>
+      <Image src={image} />
+      {category}
+      {place}
+      {writer}
+      {benefit}
+    </Wrapper>
+  );
+};
+export default CategoryPlan;
