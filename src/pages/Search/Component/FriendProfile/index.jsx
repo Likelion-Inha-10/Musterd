cc68044406b2dab0a2c27a67c942129be82cc4b3
JunHyeon <dlwnsgus07@naver.com>
2022-08-12 17:30:17 +0900

    feat: add FriendProfile Component

---
+++ src/pages/Search/Component/FriendProfile/index.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+const Wrapper = styled.div`
+  background-color: blue;
+`;
+const Image = styled.img`
+  width: 30px;
+  height: 30px;
+`;
+const FriendProfile = ({ name, image, information }) => {
+  return (
+    <Wrapper>
+      <Image src={image} />
+      {name}
+      {information}
+    </Wrapper>
+  );
+};
+export default FriendProfile;
