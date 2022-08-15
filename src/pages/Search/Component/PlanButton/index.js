f4f0fe548a12f87a392cf1709c2292d4b617caa6
JunHyeon <dlwnsgus07@naver.com>
2022-08-16 00:41:01 +0900

    feat : add fake api

--- src/pages/Search/Component/PlanButton/index.js
+++ src/pages/Search/Component/PlanButton/index.js
@@ -1,4 +1,6 @@
 import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 const Wrapper = styled.div`
   background-color: red;
@@ -7,9 +9,14 @@ const Image = styled.img`
   width: 30px;
   height: 30px;
 `;
+
 const PlanButton = (props) => {
+  const navigate = useNavigate();
+  const onClickPlan = () => {
+    // axios.post('/fiend/create/plan').then(navigate(`/friend/my-page`));
+  };
   return (
-    <Wrapper>
+    <Wrapper onClick={onClickPlan}>
       <Image src={props.profile_image} />
       {props.place}
       {props.title}
