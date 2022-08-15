a61801f7f77adce0749044c69eb2c3d05d86a24f
Jina Park <wlsdk9803@naver.com>
2022-08-15 20:18:30 +0900

    fix: routes/index.js prettier

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Main from '../pages/Main';
 import Search from '../pages/Search';
-import Login from "../pages/Login";
+import Login from '../pages/Login';
 
 // 라우트명은 kebab-case 로 작성합니다
 
