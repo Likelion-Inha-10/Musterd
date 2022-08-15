ae6a03e2c1cf588bc995c79eb40481ddc3588f69
eunseo <eundeok9@gmail.com>
2022-08-15 23:13:22 +0900

    feat: add Ranking route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Main from '../pages/Main';
 import Search from '../pages/Search';
+import Ranking from '../pages/Ranking';
 
 // 라우트명은 kebab-case 로 작성합니다
 
@@ -9,6 +10,7 @@ const Router = () => (
     <Routes>
       <Route path="/" element={<Main />} />
       <Route path="/search" element={<Search />} />
+      <Route path="/ranking" element={<Ranking />} />
     </Routes>
   </BrowserRouter>
 );
