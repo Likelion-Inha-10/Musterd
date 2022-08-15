769d29f86975ca86f4e15a5a0c76d9be981e9378
JunHyeon <dlwnsgus07@naver.com>
2022-08-16 00:07:02 +0900

    fix : fix fakeApi response with API sheet

--- src/apis/fakeResponse.js
+++ src/apis/fakeResponse.js
@@ -1,61 +1,65 @@
 export const categoryPlans = [
   {
-    id: '1',
-    category: '카테고리 설명1',
-    place: '킹타가야1',
-    writer: '플랜 만든 사람1',
-    image:
-      'https://images.unsplash.com/photo-1649892231169-8c54be4b9a79?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1288&q=80',
+    category: '운동', // 카테고리
+    plan_list: [
+      {
+        id: 1,
+        place: '하타가야1호점',
+        profile_image:
+          'https://user-images.githubusercontent.com/37991960/184546080-9222b594-76a2-4c97-9cab-65ebe4348410.JPG', //플랜 제작자의 프로필 사진
+        title: '이성인 선배님과 배드민턴치기',
+        promise_time: 123125,
+      },
+      {
+        id: 2,
+        place: '하타가야2호점',
+        profile_image:
+          'https://user-images.githubusercontent.com/37991960/184546080-9222b594-76a2-4c97-9cab-65ebe4348410.JPG', //플랜 제작자의 프로필 사진
+        title: '이성인 선배님과 축구치기',
+        promise_time: 123126,
+      },
+    ],
   },
   {
-    id: '2',
-    category: '카테고리 설명2',
-    place: '킹타가야2',
-    writer: '플랜 만든 사람2',
-    image:
-      'https://images.unsplash.com/photo-1649892231169-8c54be4b9a79?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1288&q=80',
-  },
-  {
-    id: '3',
-    category: '카테고리 설명3',
-    place: '킹타가야3',
-    writer: '플랜 만든 사람3',
-    image:
-      'https://images.unsplash.com/photo-1649892231169-8c54be4b9a79?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1288&q=80',
-  },
-  {
-    id: '4',
-    category: '카테고리 설명4',
-    place: '킹타가야4',
-    writer: '플랜 만든 사람4',
-    image:
-      'https://images.unsplash.com/photo-1649892231169-8c54be4b9a79?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1288&q=80',
+    category: '식당', //
+    plan_list: [
+      {
+        id: 3,
+        place: '찌개사랑',
+        profile_image:
+          'https://user-images.githubusercontent.com/37991960/184546080-9222b594-76a2-4c97-9cab-65ebe4348410.JPG', //플랜 제작자의 프로필 사진
+        title: '이성인 선배님과 밥약속',
+        promise_time: 123127,
+      },
+      {
+        id: 4,
+        place: '옹기종기',
+        profile_image:
+          'https://user-images.githubusercontent.com/37991960/184546080-9222b594-76a2-4c97-9cab-65ebe4348410.JPG', //플랜 제작자의 프로필 사진
+        title: '이성인 선배님과 회먹기',
+        promise_time: 123128,
+      },
+    ],
   },
 ];
 
 export const friendList = [
   {
-    id: '1',
-    name: '친구1',
-    profile_image: '',
-    information: '정보1',
+    id: 124,
+    name: '이성인',
+    profile_image: 'https://user-images.githubusercontent.com/37991960', //친구의 프로필 이미지
+    point: 126,
   },
   {
-    id: '2',
-    name: '친구1',
-    profile_image: '',
-    information: '정보2',
+    id: 146,
+    name: '이성이',
+    profile_image: 'https://user-images.githubusercontent.com/37991960',
+    point: 123,
   },
   {
-    id: '3',
-    name: '친구3',
-    profile_image: '',
-    information: '정보3',
-  },
-  {
-    id: '4',
-    name: '친구4',
-    profile_image: '',
-    information: '정보4',
+    id: 186,
+    name: '이성',
+    profile_image: 'https://user-images.githubusercontent.com/37991960',
+    point: 12,
   },
 ];
