c15317647ba38b9c49c18c1b1ed0603045332819
JunHyeon <dlwnsgus07@naver.com>
2022-08-15 22:39:53 +0900

    feat :  add simple-slick

--- src/pages/Search/Component/CategoryPlan/index.jsx
+++ src/pages/Search/Component/CategoryPlan/index.jsx
@@ -1,4 +1,8 @@
 import styled from 'styled-components';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
 const Wrapper = styled.div`
   background-color: red;
 `;
@@ -6,15 +10,26 @@ const Image = styled.img`
   width: 30px;
   height: 30px;
 `;
+
 const CategoryPlan = ({ id, category, place, writer, benefit, image }) => {
+  let settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+  };
+
   return (
-    <Wrapper>
-      <Image src={image} />
-      {category}
-      {place}
-      {writer}
-      {benefit}
-    </Wrapper>
+    <Slider>
+      <Wrapper>
+        <Image src={image} />
+        {category}
+        {place}
+        {writer}
+        {benefit}
+      </Wrapper>
+    </Slider>
   );
 };
 export default CategoryPlan;
