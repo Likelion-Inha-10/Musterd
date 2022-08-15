861da718af25157f070e8ed775dc2edf85d17551
Jina Park <wlsdk9803@naver.com>
2022-08-15 20:15:06 +0900

    fix: delete yarn and fix LoginSignupStyled prettier

--- src/musterd-ui/LoginSignupStyled.js
+++ src/musterd-ui/LoginSignupStyled.js
@@ -1,37 +1,37 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const Button = styled.button`
-  cursor: ${(props) => props.cursor || "pointer"};
+  cursor: ${(props) => props.cursor || 'pointer'};
   width: ${(props) => props.width};
   height: ${(props) => props.height};
-  border-radius: ${(props) => props.borderRadius || "5px"};
-  border: ${(props) => props.border || "none"};
+  border-radius: ${(props) => props.borderRadius || '5px'};
+  border: ${(props) => props.border || 'none'};
   font-size: ${(props) => props.fontSize};
-  font-weight: ${(props) => props.fontWeight || "normal"};
-  padding: ${(props) => props.padding || "0"};
-  margin: ${(props) => props.margin || "0"};
-  background: ${(props) => props.backgroundColor || "white"};
-  color: ${(props) => props.color || "black"};
-  float: ${(props) => props.float || "none"};
-  box-shadow: ${(props) => props.boxShadow || "none"};
+  font-weight: ${(props) => props.fontWeight || 'normal'};
+  padding: ${(props) => props.padding || '0'};
+  margin: ${(props) => props.margin || '0'};
+  background: ${(props) => props.backgroundColor || 'white'};
+  color: ${(props) => props.color || 'black'};
+  float: ${(props) => props.float || 'none'};
+  box-shadow: ${(props) => props.boxShadow || 'none'};
 `;
 
 export const TextBox = styled.div`
-  color: ${(props) => props.color || "black"};
-  font-weight: ${(props) => props.fontWeight || "normal"};
-  font-size: ${(props) => props.fontSize || "10px"};
-  float: ${(props) => props.float || "none"};
-  padding: ${(props) => props.padding || "0"};
-  line-height: ${(props) => props.lineHeight || "normal"};
-  display: ${(props) => props.display || "block"};
-  cursor: ${(props) => props.cursor || "auto"};
+  color: ${(props) => props.color || 'black'};
+  font-weight: ${(props) => props.fontWeight || 'normal'};
+  font-size: ${(props) => props.fontSize || '10px'};
+  float: ${(props) => props.float || 'none'};
+  padding: ${(props) => props.padding || '0'};
+  line-height: ${(props) => props.lineHeight || 'normal'};
+  display: ${(props) => props.display || 'block'};
+  cursor: ${(props) => props.cursor || 'auto'};
 `;
 
 export const LoginWrapper = styled.div`
   position: absolute;
   left: 50%;
-  top: ${(props) => props.top || "50%"};
-  transform: ${(props) => props.transform || "translate(-50%, -50%)"};
+  top: ${(props) => props.top || '50%'};
+  transform: ${(props) => props.transform || 'translate(-50%, -50%)'};
   text-align: center;
   padding: 10px;
   width: ${(props) => props.width};
