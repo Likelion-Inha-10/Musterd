69f356b54811f314fe6bb193365c27d723b678c5
JunHyeon <dlwnsgus07@naver.com>
2022-08-18 01:32:48 +0900

    feat : add themeProvider and basic font

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ import reset from 'styled-reset';
 import reportWebVitals from './reportWebVitals';
 import Router from './routes';
 import ReactDOMClient from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import theme from './assets/theme';
 const Wrapper = styled.div`
   width: 100%;
   height: 100%;
@@ -31,6 +33,18 @@ const GlobalStyles = createGlobalStyle`
   
   * {
     box-sizing: border-box;
+    @font-face {
+      font-family: "NotoSansKR";
+      src: url('https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100;200;300;400;500;600;700;800;900&family=Roboto:ital,wght@0,100;0,300;0,400;1,100;1,300&display=swap');
+      unicode-range: U+0041-005A, U+0061-007A;
+    }
+
+    @font-face {
+      font-family: 'Roboto Slab', serif;
+      src: url('https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100;200;300;400;500;600;700;800;900&family=Roboto:ital,wght@0,100;0,300;0,400;1,100;1,300&display=swap');
+      font-style: normal;
+    }
+    font-family: 'Roboto', sans-serif;
     font-family: 'Spoqa Han Sans Neo', 'sans-serif','Roboto Slab', serif !important;
     outline: none;
   }
@@ -39,14 +53,16 @@ const GlobalStyles = createGlobalStyle`
 const root = ReactDOMClient.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <React.StrictMode>
-      <GlobalStyles />
-      <Wrapper>
-        <Layout>
-          <Router />
-        </Layout>
-      </Wrapper>
-    </React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <React.StrictMode>
+        <GlobalStyles />
+        <Wrapper>
+          <Layout>
+            <Router />
+          </Layout>
+        </Wrapper>
+      </React.StrictMode>
+    </ThemeProvider>
   </Provider>
 );
 
