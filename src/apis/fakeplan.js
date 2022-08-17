4ed7d1f9841cf5bd289f2a95a4c56b70cc3a3a7b
JunHyeon <dlwnsgus07@naver.com>
2022-08-17 16:13:28 +0900

    fix detail for show info

--- src/apis/fakeplan.js
+++ src/apis/fakeplan.js
@@ -1,23 +1,23 @@
-export const Plans =[
+export const Plans = [
   {
-		"id": 17, // 해당 플랜의 id
-		"isDone": true, // 사용자가 완료 버튼을 눌렀는지 아닌 여부 
-		"title":"이성인 선배님과 밥 먹기로 함", //plan의 내용
-		"promise_time": 102939 ,// 약속한 날짜
-		"is_mine": true //현재 로그인한 유저가 작성한 글인지 남들의 약속을 가져온 것인지 체킹
-	},
-	{
-		"id": 18,
-		"isDone": false,
-		"title":"이성인 선배님과 밥 먹기로 함1",
-		"promise_time": 102940,
-		"is_mine": true
-	},
-	{
-		"id": 19,
-		"isDone": false,
-		"title":"이성인 선배님과 밥 먹기로 2",
-		"promise_time": 102941,
-		"is_mine": false,
-	}
+    id: 17, // 해당 플랜의 id
+    isDone: true, // 사용자가 완료 버튼을 눌렀는지 아닌 여부
+    title: '완료 선배님과 밥 먹기로 함', //plan의 내용
+    promise_time: 102939, // 약속한 날짜
+    is_mine: true, //현재 로그인한 유저가 작성한 글인지 남들의 약속을 가져온 것인지 체킹
+  },
+  {
+    id: 18,
+    isDone: false,
+    title: '미완료 이성인 선배님과 밥 먹기로 함1',
+    promise_time: 102940,
+    is_mine: true,
+  },
+  {
+    id: 19,
+    isDone: false,
+    title: '미완료 이성인 선배님과 밥 먹기로 2',
+    promise_time: 102941,
+    is_mine: false,
+  },
 ];
