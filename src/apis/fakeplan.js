11b2587f860e428e0298a116a25c9330484aebc9
SeongLee <ls777706@gmail.com>
2022-08-18 18:14:04 +0900

    feat: add MyPage Button function

--- src/apis/fakeplan.js
+++ src/apis/fakeplan.js
@@ -3,7 +3,7 @@ export const Plans = [
     id: 17, // 해당 플랜의 id
     isDone: true, // 사용자가 완료 버튼을 눌렀는지 아닌 여부
     title: '완료 선배님과 밥 먹기로 함', //plan의 내용
-    promise_time: 102939, // 약속한 날짜
+    promise_time: 102939, // 약속한 시간
     is_mine: true, //현재 로그인한 유저가 작성한 글인지 남들의 약속을 가져온 것인지 체킹
   },
   {
@@ -11,13 +11,48 @@ export const Plans = [
     isDone: false,
     title: '미완료 이성인 선배님과 밥 먹기로 함1',
     promise_time: 102940,
-    is_mine: true,
+    is_mine: false,
   },
   {
     id: 19,
     isDone: false,
     title: '미완료 이성인 선배님과 밥 먹기로 2',
     promise_time: 102941,
+    is_mine: true,
+  },
+  {
+    id: 20,
+    isDone: false,
+    title: '미완료 이성인 선배님과 밥 먹기로 2',
+    promise_time: 102941,
+    is_mine: true,
+  },
+  {
+    id: 21,
+    isDone: false,
+    title: '미완료 이성인 선배님과 밥 먹기로 2',
+    promise_time: 102941,
     is_mine: false,
+  },
+  {
+    id: 22,
+    isDone: false,
+    title: '미완료 이성인 선배님과 밥 먹기로 2',
+    promise_time: 102941,
+    is_mine: true,
+  },
+  {
+    id: 23, 
+    isDone: true, 
+    title: '완료 선배님과 밥 먹기로 함', 
+    promise_time: 102939,
+    is_mine: true,
+  },
+  {
+    id: 24,
+    isDone: false,
+    title: '미완료 이성인 선배님과 밥 먹기로 함1',
+    promise_time: 102940,
+    is_mine: true,
   },
 ];
