5b1bd33be4ca75a394f9e49862b2bfc653d5ef5e
JunHyeon <dlwnsgus07@naver.com>
2022-08-18 15:48:22 +0900

    fix : css error

--- src/pages/Search/Component/CategoryPlan/index.jsx
+++ src/pages/Search/Component/CategoryPlan/index.jsx
@@ -2,7 +2,11 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import PlanButton from '../PlanButton';
+import styled from 'styled-components';
 
+const Wrapper = styled.div`
+  margin-bottom: 2.625rem;
+`;
 const CategoryPlan = (planList) => {
   let settings = {
     dots: false,
@@ -13,22 +17,24 @@ const CategoryPlan = (planList) => {
   };
 
   return (
-    <Slider>
-      {Array.isArray(planList.planList)
-        ? planList.planList.map((plan) => {
-            console.log(plan);
-            return (
-              <PlanButton
-                key={plan.id}
-                place={plan.place}
-                title={plan.title}
-                promise_time={plan.promise_time}
-                profile_image={plan.profile_image}
-              />
-            );
-          })
-        : null}
-    </Slider>
+    <Wrapper>
+      <Slider>
+        {Array.isArray(planList.planList)
+          ? planList.planList.map((plan) => {
+              console.log(plan);
+              return (
+                <PlanButton
+                  key={plan.id}
+                  place={plan.place}
+                  title={plan.title}
+                  promise_time={plan.promise_time}
+                  profile_image={plan.profile_image}
+                />
+              );
+            })
+          : null}
+      </Slider>
+    </Wrapper>
   );
 };
 export default CategoryPlan;
