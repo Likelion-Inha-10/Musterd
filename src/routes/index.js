85f49d93b0f73840c11f9ab4ccaab7f2f16bec6e
eunseo <eundeok9@gmail.com>
2022-08-19 21:52:13 +0900

    chore: delete unused code

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,7 +8,6 @@ import QrCamera from '../pages/QrCamera';
 import MyPage from '../pages/MyPage/MyPage';
 import Stamp from '../pages/Stamp';
 import LikeLion from '../pages/Stamp/Component/StampPage/LikeLion';
-import Performance from '../pages/Stamp/Component/Performance/index';
 import Harvardcomplete from '../pages/Stamp/Component/Performance/harvardcomplete';
 import AddPopUp from '../musterd-ui/PopUpStyled/AddPopUp';
 import InhaUni from '../pages/Stamp/Component/StampPage/InhaUni';
